2f40099d57fbbcce0afb362a614f931c11fa6460
Vasco Lavos <vazkii@hotmail.com>
2016-05-21 00:00:20 +0100

    Added the recipe.

--- src/main/java/vazkii/morphtool/MorphTool.java
+++ src/main/java/vazkii/morphtool/MorphTool.java
@@ -1,11 +1,15 @@
 package vazkii.morphtool;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import vazkii.morphtool.proxy.CommonProxy;
 
 @Mod(modid = MorphTool.MOD_ID, name = MorphTool.MOD_NAME, version = MorphTool.VERSION, dependencies = MorphTool.DEPENDENCIES, guiFactory = MorphTool.GUI_FACTORY)
@@ -28,6 +32,13 @@ public class MorphTool {
 		ConfigHandler.init(event.getSuggestedConfigurationFile());
 		
 		tool = new ItemMorphTool();
+		
+		GameRegistry.addShapedRecipe(new ItemStack(tool), 
+				" GB", " IR", "I  ",
+				'G', new ItemStack(Items.DYE, 1, EnumDyeColor.GREEN.getDyeDamage()),
+				'B', new ItemStack(Items.DYE, 1, EnumDyeColor.BLUE.getDyeDamage()),
+				'R', new ItemStack(Items.DYE, 1, EnumDyeColor.RED.getDyeDamage()),
+				'I', new ItemStack(Items.IRON_INGOT));
 		
 		proxy.initModels();
 		
