2735e4c3b9822e19fc8da99954ea098f46b461b4
Vasco Lavos <vazkii@hotmail.com>
2021-01-11 11:41:51 +0000

    Fixed the same issue in akashic tome

--- src/main/java/vazkii/morphtool/ConfigHandler.java
+++ src/main/java/vazkii/morphtool/ConfigHandler.java
@@ -1,19 +1,20 @@
 package vazkii.morphtool;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
+
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public class ConfigHandler {
 
 	public static ForgeConfigSpec.BooleanValue allItems;
 	public static ForgeConfigSpec.BooleanValue invertHandShift;
-	public static ForgeConfigSpec.ConfigValue<List<String>> whitelistedItems, whitelistedNames, blacklistedMods;
-	public static ForgeConfigSpec.ConfigValue<List<String>> aliasesList;
+	public static ForgeConfigSpec.ConfigValue<List<? extends String>> whitelistedItems, whitelistedNames, blacklistedMods;
+	public static ForgeConfigSpec.ConfigValue<List<? extends String>> aliasesList;
 	static final ConfigHandler CONFIG;
 	static final ForgeConfigSpec CONFIG_SPEC;
 
@@ -26,40 +27,48 @@ public class ConfigHandler {
 	public ConfigHandler(ForgeConfigSpec.Builder builder) {
 		allItems = builder.define("Allow all items to be added", false);
 		invertHandShift = builder.define("Morph in the offhand instead of mainhand", false);
-		
-		whitelistedItems = builder.define("Whitelisted Items",
-				Arrays.asList("botania:twig_wand",
-									"appliedenergistics2:network_tool",
-									"immersiveengineering:tool",
-									"buildersguides:mallet",
-									"environmentaltech:tool_multiblock_assembler",
-									"bloodmagic:ritual_reader",
-									"draconicevolution:crystal_binder",
-									"crossroads:omnimeter"));
-		
-		whitelistedNames = builder.define("Whitelisted Names", Arrays.asList("wrench", "screwdriver", "hammer", "rotator", "configurator", "crowbar"));
-		
-		blacklistedMods = builder.define("Blacklisted Mods", Arrays.asList("tconstruct", "intangible"));
 
-		aliasesList = builder.define("Mod Aliases",
-				Arrays.asList("nautralpledge=botania",
-				"thermalexpansion=thermalfoundation",
-				"thermaldynamics=thermalfoundation",
-				"thermalcultivation=thermalfoundation", 
-				"redstonearsenal=thermalfoundation",
-				"rftoolsdim=rftools",
-				"rftoolspower=rftools",
-				"rftoolscontrol=rftools",
-				"ae2stuff=appliedenergistics2",
-				"animus=bloodmagic",
-				"integrateddynamics=integratedtunnels",
-				"mekanismgenerators=mekanism",
-				"mekanismtools=mekanism",
-				"deepresonance=rftools",
-				"xnet=rftools",
-				"buildcrafttransport=buildcraft",
-				"buildcraftfactory=buildcraft",
-				"buildcraftsilicon=buildcraft"));
+		whitelistedItems = builder.defineList("Whitelisted Items",
+				Lists.newArrayList("botania:twig_wand",
+						"appliedenergistics2:network_tool",
+						"immersiveengineering:tool",
+						"buildersguides:mallet",
+						"environmentaltech:tool_multiblock_assembler",
+						"bloodmagic:ritual_reader",
+						"draconicevolution:crystal_binder",
+						"crossroads:omnimeter"), Predicates.alwaysTrue());
+
+		whitelistedNames = builder.defineList("Whitelisted Names",
+				Lists.newArrayList("wrench", 
+						"screwdriver", 
+						"hammer", 
+						"rotator", 
+						"configurator", 
+						"crowbar"), Predicates.alwaysTrue());
+
+		blacklistedMods = builder.defineList("Blacklisted Mods",
+				Lists.newArrayList("tconstruct", 
+						"intangible"), Predicates.alwaysTrue());
+
+		aliasesList = builder.defineList("Mod Aliases",
+				Lists.newArrayList("nautralpledge=botania",
+						"thermalexpansion=thermalfoundation",
+						"thermaldynamics=thermalfoundation",
+						"thermalcultivation=thermalfoundation", 
+						"redstonearsenal=thermalfoundation",
+						"rftoolsdim=rftools",
+						"rftoolspower=rftools",
+						"rftoolscontrol=rftools",
+						"ae2stuff=appliedenergistics2",
+						"animus=bloodmagic",
+						"integrateddynamics=integratedtunnels",
+						"mekanismgenerators=mekanism",
+						"mekanismtools=mekanism",
+						"deepresonance=rftools",
+						"xnet=rftools",
+						"buildcrafttransport=buildcraft",
+						"buildcraftfactory=buildcraft",
+						"buildcraftsilicon=buildcraft"), Predicates.alwaysTrue());
 
 	}
 
